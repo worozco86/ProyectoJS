381540017b2000bbc8631bf3380ce8b7a81debce
Maxi Orozco <maxi_nemesis@hotmail.com>
2022-11-27 21:00:40 -0300

    cambios

--- app.js
+++ app.js
@@ -12,22 +12,52 @@ bienvenida()
 //CONDICONALES//
 let prenda = prompt("Elija su prenda")
 
+
 switch (prenda.toLowerCase()) {
     case "vestido":
-        alert ( cliente + " " +"Usted selecciono Vestido");
+        alert ( cliente + " " +"Usted selecciono Vestido ($6.000)");
         break;
     case "bikini":
-        alert (cliente + " " +"Usted selecciono Bikini");
+        alert (cliente + " " +"Usted selecciono Bikini ($.3000)");
         break;
     case "pantalon":
-        alert (cliente + " " +"Usted selecciono Pantalon");
+        alert (cliente + " " +"Usted selecciono Pantalon ($5.000)");
         break;
     case "chaleco":
-        alert (cliente + " " +"Usted selecciono Chaleco");
+        alert (cliente + " " +"Usted selecciono Chaleco ($4.000");
         break;
     default:
         alert ("por favor elija una prenda correcta")
         break;
 }
 
+let vestido = Number(prompt("ingrese su Saldo"))
 
+if (vestido >= 6000){
+    alert ("Gracias por su compra")
+} else{
+    alert("Su saldo no es suficiente")
+};
+
+let bikini = Number(prompt("ingrese su Saldo"))
+
+if (vestido >= 3000){
+    alert ("Gracias por su compra")
+} else{
+    alert("Su saldo no es suficiente")
+};
+
+let pantalon = Number(prompt("ingrese su Saldo"))
+
+if (vestido >= 5000){
+    alert ("Gracias por su compra")
+} else{
+    alert("Su saldo no es suficiente")
+};
+let chaleco = Number(prompt("ingrese su Saldo"))
+
+if (chaleco >= 2000){
+    alert ("Gracias por su compra")
+} else{
+    alert("Su saldo no es suficiente")
+};
