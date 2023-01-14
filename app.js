2a18447f5f8a2a1dbfe07794d88b4e4fbb40db50
Maxi Orozco <maxi_nemesis@hotmail.com>
2023-01-14 01:15:53 -0300

    mostarCarrito

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
 // VARIABLES //
 let nombreForm = document.querySelector("#nombre");
 let correoForm = document.querySelector("#correo");
-const carrito = []
-
-
+let carrito = []
 // Archivos LocalStorage //
-
-localStorage.setItem("carrito", JSON.stringify(carrito))
+const carritoLocal = JSON.parse(localStorage.getItem("carrito"));
+const addCart = () => {localStorage.setItem("carrito", JSON.stringify(carrito));};
 
 nombreForm.addEventListener("input", function () {
-    if (nombreForm.value === "") {
-      console.log("Ingrese un nombre válido");
-    }
-  })
-  
-  correoForm.addEventListener("input", function () {
-    if (correoForm.value === "") {
-      console.log("Ingrese un correo electrónico válido");
-    }
-  });
-  
-  let formulario = document.querySelector("#formulario");
-  let datosCliente = document.querySelector(".datosCliente");
+	if (nombreForm.value === "") {
+		console.log("Ingrese un nombre válido");
+	}
+});
 
-  
-  const pintarInfo = formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    datosCliente.innerHTML = `<h5> Muchas gracias ${nombreForm.value} por loguearte en nuestra pagina!!!, te enviaremos un mail a ${correoForm.value} para coordinar los detalles de tu compra.</h5></div>`;});
+correoForm.addEventListener("input", function () {
+	if (correoForm.value === "") {
+		console.log("Ingrese un correo electrónico válido");
+	}
+});
 
-    // SYNC Y AWAIT //
+let formulario = document.querySelector("#formulario");
+let datosCliente = document.querySelector(".datosCliente");
 
-    const lista = document.querySelector('#listado')
-    
-    const mostrarProd = async () => {
-    const resp = await fetch("./data.json")
-    const data = await resp.json()
-    
-    data.forEach((producto) => {
-      const card = document.createElement("card")
-            card.innerHTML = `
+const pintarInfo = formulario.addEventListener("submit", function (e) {
+	e.preventDefault();
+	datosCliente.innerHTML = `<h5> Muchas gracias ${nombreForm.value} por loguearte en nuestra pagina!!!, te enviaremos un mail a ${correoForm.value} para coordinar los detalles de tu compra.</h5></div>`;
+});
+
+// SYNC Y AWAIT //
+
+const lista = document.querySelector("#listado");
+
+const mostrarProd = async () => {
+	const resp = await fetch("./data.json");
+	const data = await resp.json();
+
+	data.forEach((producto) => {
+		const card = document.createElement("card");
+		card.innerHTML = `
             <div class="card m-2 mt-2 mb-2 g-3 align-items-center" style="width: 25rem;">
             <img src="${producto.img}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -46,32 +44,38 @@ nombreForm.addEventListener("input", function () {
             <p class="card-text text-center">${producto.precio}</p>
             <button id="${producto.id}" class="btn btnComprar btn-primary">AÑADIR AL CARRITO</button>
             </div>
-            </div>`
+            </div>`;
 
-            lista.append(card)
-            
-          })
-        }
-          
+		lista.append(card);
+	});
+	let btnComprar = document.querySelectorAll(".btnComprar");
+	console.log(btnComprar);
+	btnComprar.forEach((el) => {
+		el.addEventListener("click", (e) => {
+			agregarCarrito(e.target.id, data);
+		});
+	});
+};
+
 mostrarProd();
 
-let btnComprar = document.querySelectorAll(".btnComprar")
-btnComprar.forEach(el => {
-  el.addEventListener("click", (e) => {
-    agregarCarrito(e.target.id.data)
-  })
-})
+function agregarCarrito(id, data) {
+	console.log(data);
+	console.log(id);
 
-    
-function agregarCarrito(id, data){
-  console.log(data)
-  console.log(id)
+	const prendaElejida = data.find((el) => el.id === parseInt(id));
+	console.log(prendaElejida);
+	carrito.push(prendaElejida);
+  addCart();
 
-  const prendaElejida = data.find (el => el.id === parseInt(id));
-  console.log(prendaElejida)
-  carrito.push(prendaElejida)
 }
 
 
+const suCarrito = document.getElementById("carrito");
 
-            
+if(localStorage.getItem("carrito")){
+	console.log("esta guardado")
+}else{ 
+	console.log("no esta guardado");
+	localStorage.setItem
+}
